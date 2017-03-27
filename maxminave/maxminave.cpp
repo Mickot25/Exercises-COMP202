0b1ee2cc08e959054d4160a251ba5fd92dad317c
Mickot25 <michaelcontrerasr25@gmail.com>
2017-03-27 22:38:09 +0800

    maxminave

---
+++ maxminave/maxminave.cpp
@@ -0,0 +1,51 @@
+/*
+From a series of numbers inputted by the user, display the minimum, maximum and average of the mininum and maximum of the series of numbers.
+*/
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	double number, max, min;
+	char choice;
+
+	do
+	{
+		cout << "Input a series of numbers, press 0 to stop: "; cin >> number;
+		max = number;
+		min = number;
+
+		if (number != 0)
+		{
+			while (number != 0)
+			{
+				if (number >= max)
+					max = number;
+
+				else if (number <= min)
+					min = number;
+				cout << "Input a series of numbers, press 0 to stop: "; cin >> number;
+			}
+
+			cout << endl;
+
+			cout << "Max: " << max << endl;
+			cout << "Min: " << min << endl;
+			cout << "Ave: " << (max + min) / 2.0 << endl;
+
+			cout << endl;
+
+			cout << "Do you want to repeat the program? Press Y: "; cin >> choice;
+
+			cout << endl;
+		}
+
+		else
+			return 0;
+
+	} while (choice == 'Y' || choice == 'y');
+	_getch();
+	return 0;
+}
--- scratchpad/scratchpad.cpp
+++ scratchpad/scratchpad.cpp
@@ -1,18 +1,43 @@
-#include <iostream>
-#include <conio.h>
+#include<iostream>
+#include<conio.h>
 using namespace std;
 
 int main()
-{
-	int n, s = 0;
-	cout << "Enter a number: "; cin >> n;
 
-	while (n > 0)
-	{
-		s += (n % 10);
-		n /= 10;
-	}
-	cout << "Sum of all digits: " << s << endl;
+{
+
+	double min, max, x;
+	char ans;
+
+	do {
+		cout << "this program will compute and display the min and maximum value of input numbers" << endl;
+		cout << "Please enter series of numbers (0 to stop): ";
+		cin >> x;
+		min = x;
+		cin >> x;
+		max = x;
+		while (x != 0)
+		{
+			if (x>max)
+			{
+				max = x;
+				//gawin mong max = x
+			}
+			if (x<max)
+			{
+				min = x;
+				//gawin mong min = x
+			}
+			cin >> x;
+
+		}
+		cout << "max: " << max << endl;
+		cout << "min: " << min << endl;
+		cout << "Average is: " << (max + min) / 2.0 << endl;
+
+		cout << "If you wish to continue, press Y....: ";
+		cin >> ans;
+	} while (ans == 'Y' && ans == 'y');
 
 	_getch();
 	return 0;
